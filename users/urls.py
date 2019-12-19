46d40022b5fd232da87ce375a03149ccd8a2e8f2
André Atalaia <andreatalaia@Andres-MBP.lan>
2019-12-19 11:08:40 +0000

    Cleaned imports in user Views , Added comments to user views , Deleted unused commented code in urls

--- users/urls.py
+++ users/urls.py
@@ -7,12 +7,3 @@ urlpatterns = [
   path('', UsersList.as_view() , name = 'user-list'),
 
 ]
-
-
-# path('register/',RegisterUserView.as_view(),name= 'user'),
-    # path('users/', UserList.usersEndpoint , name = 'user-list'),
-    # path('users/', ,
-    # path('upvotes/games/<int:game_id>', UpvoteList.upvotesByGameEndpoint),
-    # path('upvotes/', UpvoteList.allupvotes, name="upvotes-list"),
-    # path('upvotes/users/<int:user_id>', UpvoteList.upvotesByUserEndpoint),
-    # path('upvotes/users/<int:user_id>/games', UpvoteList.upvotesByUserGameEndpoint),
