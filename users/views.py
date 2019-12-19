c7094036f924125857aa3cf801cae842e012c134
André Atalaia <andreatalaia@Andres-MBP.lan>
2019-12-19 11:08:40 +0000

    Cleaned imports in user Views , Added comments to user views , Deleted unused commented code in urls

--- users/views.py
+++ users/views.py
@@ -1,36 +1,44 @@
-from django.http import HttpResponse
 from django.contrib.auth.models import User
 from rest_framework import generics
-from rest_framework.response import Response
-from rest_framework.exceptions import APIException
-from django.utils.encoding import force_text
-from rest_framework import status
+from django.core import serializers
 from users.serializers import UserSerializer
 from games.serializers import GameSerializer
 from games.models import Game
-from django.utils.encoding import force_text
-from rest_framework import status
-from django.core import serializers
-from django.http import *
-from rest_framework.decorators import api_view
-from rest_framework.reverse import reverse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated,AllowAny
-import json
 
+"""
+/register/
+
+Endpoint that registers a user with the given data , in the request body
+
+Only Handles Posts Requests
+"""
 class RegisterUserView(generics.CreateAPIView):
   model = get_user_model()
   permission_classes = (AllowAny,)
   serializer_class = UserSerializer
     
-    
+"""
+/users/
+
+Endpoint that lists all registed users 
+
+Only Handles Get Requests , used for listing a collection of models
+"""
 class UsersList(generics.ListAPIView):
   model = get_user_model()
   permission_classes = (AllowAny,)
   serializer_class = UserSerializer
   queryset = User.objects.all()
+
+"""  
+/user/<int:pk>
+
+Endpoint that lists the information of a given user with that id
+
+Only Handles Get Requests, used for listing a single model of instance
+"""
 
 class UserList(generics.RetrieveAPIView):
   model = get_user_model()
