57d62d7e955fa4f98946ab7e403503bc21d10709
André Atalaia <andreatalaia@Andres-MBP.lan>
2020-01-08 11:06:09 +0000

    Adding heroku

--- tothetop/settings.py
+++ tothetop/settings.py
@@ -148,6 +148,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
