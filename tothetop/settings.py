5c67e6ac55192208ae27b9a5782c35e47763bbcb
André Atalaia <andreatalaia@Andres-MBP.lan>
2020-01-09 17:29:52 +0000

    Added new seetings for session cokkie

--- tothetop/settings.py
+++ tothetop/settings.py
@@ -153,7 +153,13 @@ CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     'to-the-top-ng.herokuapp.com',
+
 ]
+
+SESSION_COOKIE_HTTPONLY = False
+SESSION_COOKIE_SAMESITE = None
+
+
 
 import dj_database_url
 
