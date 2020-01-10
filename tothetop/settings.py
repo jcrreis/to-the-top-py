4450c8dbb0c1008a7d317cb1edfa95f2aead1b10
André Atalaia <andreatalaia@Andres-MBP.lan>
2020-01-10 18:22:45 +0000

    delete cors headers

--- tothetop/settings.py
+++ tothetop/settings.py
@@ -156,9 +156,6 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
-from corsheaders.defaults import default_headers
-
-CORS_ALLOW_HEADERS = default_headers 
 
 SESSION_COOKIE_HTTPONLY = False
 SESSION_COOKIE_SAMESITE = None
