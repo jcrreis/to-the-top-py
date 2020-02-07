35dbbbca60e885e24f41c906673b1c36c659c632
André Atalaia <andreatalaia@Andres-MBP.lan>
2020-02-07 15:41:39 +0000

    delete csrf middleware

--- tothetop/settings.py
+++ tothetop/settings.py
@@ -58,13 +58,12 @@ INSTALLED_APPS = [
     'tothetop.apps.TotheTopConfig',
     'corsheaders',
 ]
-
+   # 'django.middleware.csrf.CsrfViewMiddleware',
 MIDDLEWARE = [
         'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
