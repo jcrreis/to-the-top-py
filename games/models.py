4547adeddecc32daa0145624877c21ec80905b85
João Reis <jraposo@imaginarycloud.com>
2020-02-12 11:01:48 +0000

    Add limit 10Mb image field on models

--- games/models.py
+++ games/models.py
@@ -3,21 +3,29 @@ from users.models import User
 from upvotes.models import Upvote
 
 class Game(models.Model):
-	name = models.CharField(max_length=200)
-	price = models.FloatField()
-	description = models.CharField(max_length = 1000,blank = True)
-	storeLink = models.URLField(max_length = 300,blank = True)
-	trailerUrl = models.URLField(max_length = 300,blank = True)
-	user = models.ForeignKey(User , on_delete = models.CASCADE)
-	image = models.ImageField(upload_to='games',null=True, blank=True)
 
-	def __str__(self):
-	 return self.name
+    
+  def validate_image(fieldfile_obj):
+    filesize = fieldfile_obj.file.size
+    megabyte_limit = 10.0
+    if filesize > megabyte_limit*1024*1024:
+      raise ValidationError("Max file size is %sMB" % str(megabyte_limit))
 
-	def getName(self):
-		return self.name
+  name = models.CharField(max_length=200)
+  price = models.FloatField()
+  description = models.CharField(max_length = 1000,blank = True)
+  storeLink = models.URLField(max_length = 300,blank = True)
+  trailerUrl = models.URLField(max_length = 300,blank = True)
+  user = models.ForeignKey(User , on_delete = models.CASCADE)
+  image = models.ImageField(upload_to='games',null=True, blank=True, validators=[validate_image])
 
-	def upvotes(self):
-		return Upvote.objects.filter(game=self.id).count()
+  def __str__(self):
+    return self.name
+
+  def getName(self):
+    return self.name
+
+  def upvotes(self):
+    return Upvote.objects.filter(game=self.id).count()
 
 
--- users/models.py
+++ users/models.py
@@ -3,9 +3,14 @@ from django.db import models
 
 class User(AbstractUser):
     """User model."""
+    def validate_image(fieldfile_obj):
+      filesize = fieldfile_obj.file.size
+      megabyte_limit = 10.0
+      if filesize > megabyte_limit*1024*1024:
+        raise ValidationError("Max file size is %sMB" % str(megabyte_limit))
 
     email = models.EmailField(('email address'), unique=True)
-    image = models.ImageField(upload_to='users',null=True, blank=True)
+    image = models.ImageField(upload_to='users',null=True, blank=True, validators=[validate_image])
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
 
